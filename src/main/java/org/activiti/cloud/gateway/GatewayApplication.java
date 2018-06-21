01afc63a02080de89c17e4f7054348d9a44d3386
Ryan Dawson <ryan.dawson@alfresco.com>
2018-06-21 16:13:13 +0100

    enable cors in gw

--- src/main/java/org/activiti/cloud/gateway/GatewayApplication.java
+++ src/main/java/org/activiti/cloud/gateway/GatewayApplication.java
@@ -7,8 +7,14 @@ import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
 import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.handler.RoutePredicateHandlerMapping;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.cors.CorsConfiguration;
+
+import java.util.Arrays;
+import java.util.HashMap;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -28,4 +34,12 @@ public class GatewayApplication {
         return new DiscoveryClientRouteDefinitionLocator(discoveryClient,
                 properties);
     }
+
+    @Bean public CorsConfiguration corsConfiguration(RoutePredicateHandlerMapping routePredicateHandlerMapping) {
+        CorsConfiguration corsConfiguration = new CorsConfiguration().applyPermitDefaultValues();
+        Arrays.asList(HttpMethod.OPTIONS, HttpMethod.PUT, HttpMethod.GET, HttpMethod.DELETE, HttpMethod.POST).
+                forEach(m -> corsConfiguration.addAllowedMethod(m));
+        corsConfiguration.addAllowedOrigin("*");
+        routePredicateHandlerMapping.setCorsConfigurations(new HashMap<String, CorsConfiguration>() {{ put("/**", corsConfiguration); }});
+        return corsConfiguration; }
 }
